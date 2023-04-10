b48d661a6da0cab6a48a8d7e182593fcdf18ce35
Aditya Narayan nayak <72183256+Aditya-Narayan-Nayak@users.noreply.github.com>
2023-04-10 12:48:08 +0530

    Update install.sh

--- script/install.sh
+++ script/install.sh
@@ -3,3 +3,9 @@ az --version
 az login
 subscriptionId=$(az account show --query id --output tsv)
 az ad sp create-for-rbac -n "<Unique SP Name>" --role "Contributor" --scopes /subscriptions/$subscriptionId
+az provider register --namespace Microsoft.Kubernetes
+az provider register --namespace Microsoft.KubernetesConfiguration
+az provider register --namespace Microsoft.ExtendedLocation
+az extension add --name connectedk8s
+az extension add --name k8s-configuration
+sudo snap install microk8s --classic
